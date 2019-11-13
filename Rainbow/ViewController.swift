f27d17a6258bd9b2b7ed51915f83278fe0df94b2
morgan smith <mssmitty07@gmail.com>
2019-11-12 21:47:15 -0500

    initial rainbow commit

---
+++ Rainbow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by Morgan Smith on 11/12/19.
+//  Copyright © 2019 Morgan Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
